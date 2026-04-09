880ee6288127e9989c79abc8b025e832ff22d1f3
eimmig <eduardo-immig@hotmai.com>
2026-04-09 19:32:58 -0300

    commit projeto

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -5,13 +5,6 @@ import uiRoutes from './routes/ui-routes.ts'
 
 const app = new Hono()
 
-app.use('*', async (c, next) => {
-  // Marker headers to verify requests are served by this Edge Function.
-  c.header('x-edge-function', 'index')
-  c.header('x-edge-runtime', 'netlify-edge')
-  await next()
-})
-
 app.route('/', uiRoutes)
 app.route('/tasks', taskRoutes)
 
